b0a730620250ef876e9d2e81e6537c4e65e2f5bb
Matthew Gleich <matthewgleich@gmail.com>
2020-01-19 13:57:57 -0500

    Create push-latest.sh

---
+++ push-latest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm .zshrc
+rm .bashrc
+rm .bash_profile
+rm .vimrc
+rm Brewfile
+
+cwd=$(pwd)
+cd ~
+rm Brewfile
+brew bundle dump
+cd $cwd
+
+cp ~/.zshrc .zshrc
+cp ~/.vimrc .vimrc
+cp ~/.bashrc .bashrc
+cp ~/.bash_profile .bash_profile
+cp ~/Brewfile Brewfile
+
+git add .
+git commit -m "Update to latest"
+git push
