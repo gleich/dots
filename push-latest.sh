54cb7078ab08e2343756d50f81f97f92cc273353
Matthew Gleich <matthewgleich@gmail.com>
2020-01-22 22:10:45 -0500

    Use get-latest.sh

--- push-latest.sh
+++ push-latest.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-rm .zshrc
-rm .bashrc
-rm .bash_profile
-rm .vimrc
-rm Brewfile
-
-cwd=$(pwd)
-cd ~
-rm Brewfile
-brew bundle dump
-cd $cwd
-
-cp ~/.zshrc .zshrc
-cp ~/.vimrc .vimrc
-cp ~/.bashrc .bashrc
-cp ~/.bash_profile .bash_profile
-cp ~/Brewfile Brewfile
+sh get-latest.sh
 
 git add .
 git commit -m "Update to latest"
