9efe7fc8fafc2534ac845ee4fd9689e558484fcb
Matthew Gleich <matthewgleich@gmail.com>
2020-01-22 22:10:28 -0500

    Remove .bashrc

--- get-latest.sh
+++ get-latest.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 rm .zshrc
-rm .bashrc
-rm .bash_profile
 rm .vimrc
 rm Brewfile
 
@@ -14,7 +12,5 @@ cd $cwd
 
 cp ~/.zshrc .zshrc
 cp ~/.vimrc .vimrc
-cp ~/.bashrc .bashrc
-cp ~/.bash_profile .bash_profile
 cp ~/Brewfile Brewfile
 
