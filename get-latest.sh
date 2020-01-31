028ab1cbfd476bd1a5bfa746a0a8069944338481
Matthew Gleich <matthewgleich@gmail.com>
2020-01-30 22:56:30 -0500

    Add VSCode settings

--- get-latest.sh
+++ get-latest.sh
@@ -5,12 +5,14 @@ rm -rf zsh
 rm -rf vim
 rm -rf homebrew
 rm -rf flutter
+rm -rf VSCode
 
 # Making new folders
 mkdir zsh
 mkdir vim
 mkdir homebrew
 mkdir flutter
+mkdir VSCode
 
 # Generating Brewfile
 cwd=$(pwd)
@@ -24,3 +26,4 @@ cp ~/.zshrc ./zsh/.zshrc
 cp ~/.vimrc ./vim/.vimrc
 cp ~/Brewfile ./homebrew/Brewfile
 cp ~/.flutter_settings ./flutter/.flutter_settings
+cp ~/Library/Application' 'Support/Code/User/settings.json ./VSCode/settings.json
