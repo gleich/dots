08af44f2f38d450f8f106f1553f1b464d8f09357
Matthew Gleich <email@mattglei.ch>
2020-12-25 15:36:47 -0500

    🔧 Update to latest

--- bin/fetch/main.py
+++ bin/fetch/main.py
@@ -80,6 +80,9 @@ def main():
         ],
     )
     shutil.rmtree(os.path.join(".", USR_NAME, ".config", "nvim", "autoload"))
+    os.system(
+        "xmllint --format matt/.config/StardewValley/startup_preferences >> matt/.config/StardewValley/startup_preferences"
+    )
     write_to_readme()
     push_changes()
 
