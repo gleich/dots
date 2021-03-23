47fa8ae1676c7149db9d7bbd134b0603cdabed53
Matthew Gleich <git@mattglei.ch>
2021-03-23 11:25:14 -0400

    refactor(dev script): Refactor commit message to conventional commits
    See https://www.conventionalcommits.org/ for more information
    
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- bin/fetch/main.py
+++ bin/fetch/main.py
@@ -88,7 +88,7 @@ def main():
     )
     shutil.rmtree(os.path.join(".", USR_NAME, ".config", "nvim", "autoload"))
     write_to_readme()
-    # push_changes()
+    push_changes()
 
 
 def ensure_correct_path():
@@ -160,7 +160,7 @@ def push_changes():
     """Push the latest changes"""
     logger.info("Pushing the latest changes")
     os.system("git add .")
-    os.system('git commit -m "🔧 Update to latest"')
+    os.system('git commit -m "chore(update): Update to latest config"')
     os.system("git push")
     logger.success("Pushed the latest changes!")
 
