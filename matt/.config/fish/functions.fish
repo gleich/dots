eb461b9919c4b01f076b8e45f1755a879ba50bc8
Matt Gleich <git@mattglei.ch>
2023-01-25 09:00:26 -0500

    chore(update): update to latest config

--- matt/.config/fish/functions.fish
+++ matt/.config/fish/functions.fish
@@ -5,3 +5,9 @@ end
 function ccat
 	cat $argv | pbcopy
 end
+
+function backup_remarkable
+    cd "/Users/matt/Library/Mobile Documents/com~apple~CloudDocs/Important/exports"
+    cd remarkable
+    scp -r root@10.11.99.1:/home/root/.local/share/remarkable/xochitl "remarkable-$(date -I)"
+end
