43d6b0a51e131a6ca67725129784a9f616fdc8d6
Matt Gleich <git@mattglei.ch>
2024-09-12 09:49:50 -0400

    chore(update): update to latest config

--- matt/.config/fish/functions/s.fish
+++ matt/.config/fish/functions/s.fish
@@ -1,10 +1,10 @@
 function s
     if count $argv > /dev/null
-        set src_path ~/src/$argv
+        set src_path ~/src/gleich/$argv
     else
         set src_path ~/src/
     end
     cd $src_path
 end
 
-complete -c s -a "(command ls ~/src)"
+complete -c s -a "(command ls ~/src/gleich)"
