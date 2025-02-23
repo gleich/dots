2641497bc9b3a4d5920a8a743405622dcb5bbf16
Matt Gleich <git@mattglei.ch>
2025-02-23 01:06:26 -0500

    feat: remove bin
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- bin/install/vscode-extensions.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.chdir("../../matt/packages/vscode")
-
-with open("extensions.txt") as ext_file:
-    lines = ext_file.readlines()
-    for line in lines:
-        os.system(f"code --install-extension {line}")
