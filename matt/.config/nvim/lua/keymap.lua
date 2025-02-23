b1031914a755e3a00e8498b9b820674d7035e449
Matt Gleich <git@mattglei.ch>
2025-02-22 22:57:57 -0500

    chore(update): update to latest config

--- matt/.config/nvim/lua/keymap.lua
+++ matt/.config/nvim/lua/keymap.lua
@@ -13,8 +13,16 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "move to window down" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "move to window up" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "move to window right" })
 
--- create splits with ctrl+arrow_key
-vim.keymap.set("n", "<leader>w", "<cmd>aboveleft split<CR>", { desc = "open split above" })
-vim.keymap.set("n", "<leader>s", "<cmd>belowright split<CR>", { desc = "open split below" })
-vim.keymap.set("n", "<leader>d", "<cmd>leftabove vsplit<CR>", { desc = "open vertical split left" })
-vim.keymap.set("n", "<leader>a", "<cmd>rightbelow vsplit<CR>", { desc = "open vertical split right" })
+-- create splits with ctrl+arrow_key and switch focus to the new split
+vim.keymap.set("n", "<leader>w", "<cmd>aboveleft split | wincmd w<CR>", { desc = "open split above and move" })
+vim.keymap.set("n", "<leader>s", "<cmd>belowright split | wincmd w<CR>", { desc = "open split below and move" })
+vim.keymap.set("n", "<leader>d", "<cmd>leftabove vsplit | wincmd w<CR>", { desc = "open vertical split left and move" })
+vim.keymap.set(
+	"n",
+	"<leader>a",
+	"<cmd>rightbelow vsplit | wincmd w<CR>",
+	{ desc = "open vertical split right and move" }
+)
+
+-- create a new tab
+vim.keymap.set("n", "<leader>t", "<cmd>tabnew<cr>", { desc = "create a new tab" })
