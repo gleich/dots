22d3731d96bccda02930e8bc496c572d2a895783
Matt Gleich <git@mattglei.ch>
2025-08-21 10:49:24 -0400

    chore: update

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 updated with [gleich/scripts/dots](https://github.com/gleich/scripts/tree/main/dots). my system configuration files.
 
 ```txt
-matt@MacBookPro-541.lan 
+matt@MacBookPro-129.lan 
 ----------------------- 
 OS: macOS 15.6 arm64 
 Host: MacBookPro18,4 
---
+++ matt/.config/raycast/script-commands/turn-off-receiver.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Turn off Receiver
+# @raycast.mode silent
+# @raycast.packageName Music
+
+# Documentation:
+# @raycast.description Turned off my Yamaha R-N800A Receiver
+
+receiver_address="192.168.1.39"
+
+# turn off receiver
+curl --silent --output /dev/null "http://${receiver_address}/YamahaExtendedControl/v1/main/setPower?power=standby"
+
+echo "Turned off Yamaha R-N800A"
